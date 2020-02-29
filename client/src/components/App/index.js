cf8e2d9feb96ca82253a8d78cdcd639dfaba948b
Brendan Berry <53982094+Pickingberries@users.noreply.github.com>
2020-02-29 10:47:21 -0800

    fixed routing issues, able to see signin and signup pages

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ThemeProvider } from 'material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/core/styles'
 
-import theme from '../utils/themeUtil';
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+import theme from '../../utils/themeUtil';
+import SignIn from '../../pages/SignIn';
+import SignUp from '../../pages/SignUp';
 
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <ThemeProvider theme={ theme }>
+          <Switch>
+            <Route exact path='/' component={ SignIn }></Route>
+            <Route exact path='/signup' component={ SignUp }></Route>
+          </Switch>
+        </ThemeProvider>
+      </Router>
+      
+    );
+  }
+}
+
+
+/*
 class App extends Component {
   render() {
     return (
@@ -22,5 +40,5 @@ class App extends Component {
     );
   }
 }
-
+*/
 export default App;
