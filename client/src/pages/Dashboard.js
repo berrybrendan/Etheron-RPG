3171156462aab92048b25e5be15c2d73a928efd5
Carlos <c2rojas75@gmail.com>
2020-03-03 21:31:15 -0800

    edited the dashboard and added logout feature

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,31 +1,32 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
-
+import { useHistory } from "react-router-dom";
 import theme from '../utils/themeUtil';
 import Button from '../components/common/Button/index'
+import ButtonAppBar from '../components/common/AppBar/index'
+import Selector from '../components/common/Selector/index'
+import SimpleCard from '../components/common/Card/index'
+
+import Auth from '../utils/Auth'
 
 function Copyright() {
   return (
-    <ThemeProvider theme={ theme }>
-        <Typography variant="body2" color="textSecondary" align="center">
+    <ThemeProvider theme={theme}>
+      <Typography variant="body2" color="textSecondary" align="center">
         {'Copyright © '}
         <Link color="inherit" href="http://etheron-rpg.herokuapp.com/">
-            Etheron RPG
+          Etheron RPG
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
-        </Typography>
+      </Typography>
     </ThemeProvider>
   );
 }
@@ -61,78 +62,85 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-function Dashboard() {
+function Dashboard(props) {
+  let history = useHistory();
   const classes = useStyles();
+  const number = [5,6,7,8,9];
+  const character = {
+    name: 'Bill',
+    type: 'Warrior',
+    gold: '3000'
+
+  }
+
+  const logout = () => {
+
+    Auth.deauthenticateUser();
+    props.toggleAuthStatus()
+
+    history.push("/")
+
+  }
+
+
 
   return (
-    <ThemeProvider theme={ theme }>
-        <Grid container component="main" className={classes.root} >
+    <ThemeProvider theme={theme}>
+      <ButtonAppBar logout={logout} />
+      <Grid container component="main" className={classes.root} spacing={3} >
         <CssBaseline />
-        <Grid item xs={false} sm={4} md={7} className={classes.image} />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-            <div className={classes.paper}>
-            <Avatar className={classes.avatar}>
-                <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-                Sign in
-            </Typography>
-            <h1>You Made it!!!!</h1>
-            <form className={classes.form} noValidate>
-                <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                />
-                <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                />
-                <FormControlLabel
-                control={<Checkbox value="remember" color="secondary" />}
-                label="Remember me"
-                />
-                <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="secondary"
-                className={classes.submit}
-                >
-                Sign In
+
+
+        {/* Start of Left Side */}
+        <Grid item xs={12} sm={12} md={6} >
+          <Container>
+          <Grid container spacing={2}>
+          <Grid item xs={7} sm={7} md={7}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="secondary"
+              className={classes.submit}
+            >
+              New Character
                 </Button>
-                <Grid container>
-                <Grid item xs>
-                    <Link href="#" variant="body2">
-                    Forgot password?
-                    </Link>
-                </Grid>
-                <Grid item>
-                    <Link href="/signup" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                    </Link>
-                </Grid>
-                </Grid>
-                <Box mt={5}>
-                <Copyright />
-                </Box>
-            </form>
-            </div>
+          </Grid>
+          <Grid item xs={5} sm={5} md={5}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="secondary"
+              className={classes.submit}
+            >
+              Delete
+                </Button>
+          </Grid>
+          </Grid>
+          <Selector number={number} />
+          <Grid container spacing={2}>
+          <Grid item xs={8} sm={8} md={8}>
+           
+          </Grid>
+          <Grid item xs={4} sm={4} md={4}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="secondary"
+              className={classes.submit}
+            >
+              Start
+                </Button>
+          </Grid>
+          </Grid>
+          </Container>
         </Grid>
+        
+
+{/* Start of Right Side */}
+        <Grid item xs={12} sm={12} md={6} >
+          <SimpleCard character={character}/>
         </Grid>
+      </Grid>
     </ThemeProvider>
   );
 }
